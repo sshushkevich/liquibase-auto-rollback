501d61a3274a3cdb83bb792939e9ad3957a9f6fc
Sergei Shushkevich <s.shushkevich@gmail.com>
2025-05-26 20:20:34 -0700

    Add copyright header

--- src/main/java/com/whisperinggarden/lar/LiquibaseRollbackUtils.java
+++ src/main/java/com/whisperinggarden/lar/LiquibaseRollbackUtils.java
@@ -1,3 +1,8 @@
+/*
+ * Copyright (c) 2025 Sergei Shushkevich
+ * Licensed under the MIT License. See LICENSE file in the project root for full license information.
+ */
+
 package com.whisperinggarden.lar;
 
 import liquibase.ChecksumVersion;
